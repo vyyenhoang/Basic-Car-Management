832cfa8f0210543532a1983cec351c1f25791f4a
Vy <200408803@student.georgianc.on.ca>
2019-10-05 23:34:46 -0400

    Import project to github

---
+++ FinalProject/Controllers/CarModelsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalProject.Models;
+
+namespace FinalProject.Controllers
+{
+    public class CarModelsController : Controller
+    {
+        private finalprojectEntities db = new finalprojectEntities();
+
+        // GET: CarModels
+        public ActionResult Index()
+        {
+            var carModels = db.CarModels.Include(c => c.VehicleType);
+            return View(carModels.ToList());
+        }
+
+        // GET: CarModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CarModel carModel = db.CarModels.Find(id);
+            if (carModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carModel);
+        }
+
+        // GET: CarModels/Create
+        public ActionResult Create()
+        {
+            ViewBag.VehicleTypeId = new SelectList(db.VehicleTypes, "VehicleTypeId", "Name");
+            return View();
+        }
+
+        // POST: CarModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ModelId,EngineSize,NumberOfDoors,Colour,VehicleTypeId,CreateDate,EditDate")] CarModel carModel)
+        {
+            if (ModelState.IsValid)
+            {
+                carModel.CreateDate = DateTime.Now;
+                db.CarModels.Add(carModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.VehicleTypeId = new SelectList(db.VehicleTypes, "VehicleTypeId", "Name", carModel.VehicleTypeId);
+            return View(carModel);
+        }
+
+        // GET: CarModels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CarModel carModel = db.CarModels.Find(id);
+            if (carModel == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.VehicleTypeId = new SelectList(db.VehicleTypes, "VehicleTypeId", "Name", carModel.VehicleTypeId);
+            return View(carModel);
+        }
+
+        // POST: CarModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ModelId,EngineSize,NumberOfDoors,Colour,VehicleTypeId,CreateDate,EditDate")] CarModel carModel)
+        {
+            if (ModelState.IsValid)
+            {
+                carModel.EditDate = DateTime.Now;
+                db.Entry(carModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.VehicleTypeId = new SelectList(db.VehicleTypes, "VehicleTypeId", "Name", carModel.VehicleTypeId);
+            return View(carModel);
+        }
+
+        // GET: CarModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CarModel carModel = db.CarModels.Find(id);
+            if (carModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carModel);
+        }
+
+        // POST: CarModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CarModel carModel = db.CarModels.Find(id);
+            db.CarModels.Remove(carModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
